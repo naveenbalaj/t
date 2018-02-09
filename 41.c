2875752f57b4fae222966f25332fe4d881c33df9
naveenbalaji <30828376+naveenbalaj@users.noreply.github.com>
2018-02-09 09:31:54 +0000

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[100]=("vhjgdfjkhuihtre89");
+char b[100]=("njdfjkbnkjsdljn"),s;
+s=strcat(a,b);
+printf("%C",s);
+    return 0;
+}
+
+
+
